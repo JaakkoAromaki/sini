f390b86166be95378a7e353e7ba694aedcc3f95c
Hamlak <jaakko.aromaki@gmail.com>
2026-04-21 16:30:55 +0300

    added_tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,28 @@
+'use client';
+import { useState } from 'react';
+
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <main>
-      <div class="">
-        <div class="text cont">
-            <h1 class="text">avaa kirje</h1>
-            <button class="uusikirje">uusi kirje</button>
-            <div class="envelope pulse">
-                <div class="flap" onclick="openKirje()"></div>
-            </div>
-            <div class="letter">
-                <h1 class="header">moi sini/lumi❤️</h1>
-                <p class="letter_text">joo moikka sini! aika kuluu ihan sairaan nopee ja viimeset 3 kuukautta on kulunu ihan sairaan nopee! joka päivä mietin miten jaskat olla mun kanssa ja miten olen saanut sinut mun siunaukseks. oot ku päivänsäde, valonsäde joka valaisee mun jok ikisen päivän. oon ihan sairaan kiitollinen susta ja toivon että voit pysyy mun kanssa aina &lt;3. oot mailman ihanin ja rakastan sua enemmän ku tiedätkään  <br> <br> ps. oot täydellinen </p>
-            </div>
+    <div>
+      <div className="text cont">
+        <h1 className="text">avaa kirje</h1>
+        <button className="uusikirje" onClick={() => setIsOpen(false)}>uusi kirje</button>
+        <div className={`envelope ${isOpen ? '' : 'pulse'}`}>
+          <div className="flap" onClick={() => setIsOpen(true)}></div>
         </div>
-    </main>
+        {isOpen && (
+          <div className="letter">
+            <h1 className="header">moi sini/lumi ❤️</h1>
+            <p className="letter_text">
+              joo moikka sini! aika kuluu ihan sairaan nopee...
+              <br /><br />
+              ps. oot täydellinen
+            </p>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
