f80bc7c4c420c204853ed53f9996e5dfb6c12bbe
Hamlak <jaakko.aromaki@gmail.com>
2026-04-21 16:24:09 +0300

    added_tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
     <main>
-      <div>Next on GitHub Pages</div>
+      <div class="">
+        <div class="text cont">
+            <h1 class="text">avaa kirje</h1>
+            <button class="uusikirje">uusi kirje</button>
+            <div class="envelope pulse">
+                <div class="flap" onclick="openKirje()"></div>
+            </div>
+            <div class="letter">
+                <h1 class="header">moi sini/lumi❤️</h1>
+                <p class="letter_text">joo moikka sini! aika kuluu ihan sairaan nopee ja viimeset 3 kuukautta on kulunu ihan sairaan nopee! joka päivä mietin miten jaskat olla mun kanssa ja miten olen saanut sinut mun siunaukseks. oot ku päivänsäde, valonsäde joka valaisee mun jok ikisen päivän. oon ihan sairaan kiitollinen susta ja toivon että voit pysyy mun kanssa aina &lt;3. oot mailman ihanin ja rakastan sua enemmän ku tiedätkään  <br> <br> ps. oot täydellinen </p>
+            </div>
+        </div>
     </main>
   );
 }
